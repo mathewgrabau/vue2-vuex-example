bda9e4e47ef822a55d43171afb74c171d40389ab
Mathew Grabau <mgrabau@mathewgrabau.ca>
2021-05-16 21:37:30 -0500

    Added state, demonstrating accessing it from the state.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>
+      My name is {{ $store.state.name }} and my favorite color is {{ $store.state.favoriteColor }}. My profession is {{ $store.state.profession }}.
+    </p>
+    <hello-world />
   </div>
 </template>
 
